8512e894ed6faceab9659db813752a9e75457bad
Борис Мельников <amonsat@gmail.com>
2019-04-02 13:22:28 +0300

    Add Int64, Float64, Uint, Uint64

--- env.go
+++ env.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+// GetString - get string envirovment variable or default value
 func GetString(key, def string) string {
 	value, ok := os.LookupEnv(key)
 	if !ok {
@@ -14,6 +15,7 @@ func GetString(key, def string) string {
 	return value
 }
 
+// GetBool - get bool envirovment variable or default value
 func GetBool(key string, def bool) bool {
 	value, ok := os.LookupEnv(key)
 	if !ok {
@@ -28,7 +30,23 @@ func GetBool(key string, def bool) bool {
 	return b
 }
 
-func GetInt(key string, def int64) int64 {
+// GetInt - get int envirovment variable or default value
+func GetInt(key string, def int) int {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return def
+	}
+
+	return i
+}
+
+// GetInt64 - get int64 envirovment variable or default value
+func GetInt64(key string, def int64) int64 {
 	value, ok := os.LookupEnv(key)
 	if !ok {
 		return def
@@ -42,7 +60,23 @@ func GetInt(key string, def int64) int64 {
 	return i
 }
 
-func GetFloat(key string, def float64) float64 {
+// GetFloat32 - get float envirovment variable or default value
+func GetFloat32(key string, def float32) float32 {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+
+	f, err := strconv.ParseFloat(value, 32)
+	if err != nil {
+		return def
+	}
+
+	return float32(f)
+}
+
+// GetFloat64 - get float64 envirovment variable or default value
+func GetFloat64(key string, def float64) float64 {
 	value, ok := os.LookupEnv(key)
 	if !ok {
 		return def
@@ -54,4 +88,34 @@ func GetFloat(key string, def float64) float64 {
 	}
 
 	return f
+}
+
+// GetUint - get uint envirovment variable or default value
+func GetUint(key string, def uint) uint {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+
+	i, err := strconv.ParseUint(value, 10, 32)
+	if err != nil {
+		return def
+	}
+
+	return uint(i)
+}
+
+// GetUint64 - get uint64 envirovment variable or default value
+func GetUint64(key string, def uint64) uint64 {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+
+	i, err := strconv.ParseUint(value, 10, 64)
+	if err != nil {
+		return def
+	}
+
+	return i
 }
